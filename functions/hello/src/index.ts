d8adae8c0877f3fc0995af1e9b07bddb6c3482e3
naoyoshi.okamae <naoyoshi.okamae@livesense.co.jp>
2016-12-01 19:46:29 +0900

    wip

--- functions/hello/src/index.ts
+++ functions/hello/src/index.ts
@@ -7,45 +7,44 @@ import split = require('split')
 export default async function(request, ctx, cb) {
   await setup()
   const { byoyomi, position } = request
+  setTimeout(() => { ctx.done('timeout') }, byoyomi + 5000)
 
   const gikou = spawn('./gikou', [], { cwd: '/tmp/' })
-  gikou.stdin.write(
-    generateCommand(byoyomi, position)
-  )
-  setTimeout(() => { ctx.done('timeout') }, byoyomi + 5000)
+  gikou.stdin.write(generateCommand(byoyomi, position))
 
   const result = await getResult(gikou.stdout)
   return ctx.done(null, result)
-  // gikou.stderr.on('data', data => {
-  //   ctx.done(data)
-  // })
 }
 
-async function getResult(stdout: NodeJS.ReadableStream) {
+function getResult(stdout: NodeJS.ReadableStream) {
   let infoList = []
+  const generateResult = bestmove => ({
+    bestmove,
+    bestpv: selectBestPv(infoList),
+    info_list: infoList
+  })
   return new Promise(resolve => {
     stdout.pipe(split()).on('data', data => {
       const line: string = data.toString()
-      console.log(line)
       const [cmd, ...words] = line.split(" ")
-      if (cmd == "info") {
-        infoList.push(parseInfo(words))
-      }
-      if (cmd == "bestmove") {
-        const bestmove = words[0]
-        const bestpv = infoList.reduce((bestinfo, info) => {
-          const depth         = bestinfo["depth"] || 0
-          const seldepth      = bestinfo["seldepth"] || 0
-          const curr_depth    = info["depth"] || 0
-          const curr_seldepth = info["seldepth"] || 0
-          if (depth < curr_depth || (depth == curr_depth && seldepth < curr_seldepth)) {
-            return info
-          }
-          return bestinfo
-        })
-        resolve({ bestmove, bestpv, info_list: infoList })
-      }
+      console.log(line)
+
+      if (cmd == "info") infoList.push(parseInfo(words))
+      if (cmd == "bestmove") resolve(generateResult(words[0]))
     })
+  })
+}
+
+function selectBestPv(infoList: any[]) {
+  return infoList.reduce((bestinfo, info) => {
+    const depth         = bestinfo["depth"] || 0
+    const seldepth      = bestinfo["seldepth"] || 0
+    const curr_depth    = info["depth"] || 0
+    const curr_seldepth = info["seldepth"] || 0
+    if (depth < curr_depth || (depth == curr_depth && seldepth < curr_seldepth)) {
+      return info
+    }
+    return bestinfo
   })
 }
 
